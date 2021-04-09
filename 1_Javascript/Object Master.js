9acd04490b616aec3fcc36214b504cae24520b5a
FelipeLeytonC <hleyton@e-contact.cl>
2021-04-09 18:25:12 -0400

    Object Master

---
+++ 1_Javascript/Object Master.js
@@ -0,0 +1,49 @@
+const pokémon = Object.freeze([
+        { "id": 1,   "name": "Bulbasaur",  "types": ["poison", "grass"] },
+        { "id": 5,   "name": "Charmeleon", "types": ["fire"] },
+        { "id": 9,   "name": "Blastoise",  "types": ["water"] },
+        { "id": 12,  "name": "Butterfree", "types": ["bug", "flying"] },
+        { "id": 16,  "name": "Pidgey",     "types": ["normal", "flying"] },
+        { "id": 23,  "name": "Ekans",      "types": ["poison"] },
+        { "id": 24,  "name": "Arbok",      "types": ["poison"] },
+        { "id": 25,  "name": "Pikachu",    "types": ["electric"] },
+        { "id": 37,  "name": "Vulpix",     "types": ["fire"] },
+        { "id": 52,  "name": "Meowth",     "types": ["normal"] },
+        { "id": 63,  "name": "Abra",       "types": ["psychic"] },
+        { "id": 67,  "name": "Machamp",    "types": ["fighting"] },
+        { "id": 72,  "name": "Tentacool",  "types": ["water", "poison"] },
+        { "id": 74,  "name": "Geodude",    "types": ["rock", "ground"] },
+        { "id": 87,  "name": "Dewgong",    "types": ["water", "ice"] },
+        { "id": 98,  "name": "Krabby",     "types": ["water"] },
+        { "id": 115, "name": "Kangaskhan", "types": ["normal"] },
+        { "id": 122, "name": "Mr. Mime",   "types": ["psychic"] },
+        { "id": 133, "name": "Eevee",      "types": ["normal"] },
+        { "id": 144, "name": "Articuno",   "types": ["ice", "flying"] },
+        { "id": 145, "name": "Zapdos",     "types": ["electric", "flying"] },
+        { "id": 146, "name": "Moltres",    "types": ["fire", "flying"] },
+        { "id": 148, "name": "Dragonair",  "types": ["dragon"] }
+    ]);
+
+const div3 = pokémon.filter( val => val.id % 3 === 0)
+console.log(div3)
+
+const fuego = pokémon.filter( val => val.types.includes('fire'))
+console.log(fuego)
+
+const more2types = pokémon.filter( val => val.types.length > 1)
+console.log(more2types)
+
+const names = pokémon.filter(val => val.name).map( val => val.name );
+console.log(names)
+
+const more99 = pokémon.filter(val => val.id > 99).map( val => val.name );
+console.log(more99)
+
+const poison = pokémon.filter(val => val.types == 'poison').map( val => val.name );
+console.log(poison)
+
+const second = pokémon.filter(val => val.types[1] == 'flying').map( val => val.types[0]);
+console.log(second)
+
+const normal = pokémon.filter( val => val.types.includes('fire')).map( val => val.name);
+console.log(normal.length)
